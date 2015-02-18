7df208e98745d8a23b97665de0f1722ca64cdafd
Taylor Buley <buley@outlook.com>
2015-02-17 22:22:40 -0800

    ..

--- scrape.php
+++ scrape.php
@@ -35,10 +35,12 @@ function handle_item($filename, $basedir = '') {
 	$end = false;
 	$ps = array();
 	$current = false;
+	$cleaned = trim(str_replace( array("\x0D\x03", "\x0D", "\x03", "\xD2", "\xF5\xF5", "\xF5","\x07"), array("\n", "\n", "", '"', '"',"'","\n"), $raw));
+        $cleaned = str_replace( array("\xD0", "\xF2", "\xF3", "\xF4","`", "\xF5", "\xD2\xD5", "\xD5\xD5", "\xD5", "\xD3", "\xD1", "\xD4", "\xA9", "\x2E", "\xA5", "\x8E", "\xFB"), array('--', '"', '"', '"', "'", "'", '"', '"', "'", '"', '--', "'", "(c)", ".", "-", "é", ""), $cleaned );
+	$cleaned = str_replace( "''", '"', $cleaned );
 
-	$cleaned = trim(str_replace( array("\x0D\x03", "\x0D", "\x03", "\xD2", "\xF5\xF5", "\xF5"), array("\n", "\n", "", '"', '"',"'"), $raw));
-	$cleaned = str_replace( array("\xD0", "\xF2", "\xF3", "\xF4","`", "\xF5", "\xD2\xD5", "\xD5\xD5", "\xD5", "\xD3", "\xD1", "\xD4"), array('--', '"', '"', '"', "'", "'", '"', '"', "'", '"', '--', "'"), $cleaned );
-
+	$d = array( 'File' => "$basedir/$filename" );
+	
 	if ( false === strpos($cleaned,"Date:")) {
 		$bottom = substr($cleaned, strpos($cleaned, "0002"));
 		$rest = substr($bottom, 0, strrpos($bottom, "0004"));
@@ -49,7 +51,7 @@ function handle_item($filename, $basedir = '') {
 			$piece = substr($item, strpos($item, ",") + 1);
 			$piece = explode( "0004", $piece )[0];
 			
-			if ( false === strpos($piece,"000") && 0 !== strpos( $piece, "0" )) {
+			if ( false === strpos($piece,"0000") && 0 !== strpos( $piece, "0" )) {
 				$zs = explode("\n", strtr( $piece, array("\x03" => "\n", "\x0D" => "" ) ) ); //preg_replace('/[\x00-\x1F\x80-\xFF]/', '', strtr( $piece, array("\x03" => "\n", "\x0d" => "\r" ) ) ) );
 			}
 			if (count($zs) > 1){
@@ -64,6 +66,35 @@ function handle_item($filename, $basedir = '') {
 			echo "SKIP $basedir/$filename\n";
 		}
 		
+                foreach($os as $oi => $o) {
+                        if ( preg_match("/^Cox News Service$/i",$o) && $oi < 5 ) {
+                        	$d[ 'Author' ] = $o;
+				unset($os[$oi]);
+                        } else if ( preg_match("/^Scripps Howard News Service$/i",$o) && $oi < 5 ) {
+                        	$d[ 'Author' ] = $o;
+				unset($os[$oi]);
+                        } else if ( preg_match("/^The Associated Press$/i",$o) && $oi < 5 ) {
+                        	$d[ 'Author' ] = $o;
+				unset($os[$oi]);
+                        } else if ( preg_match("/^By\s/i",$o) && $oi < 5) {
+                                if ( 0 === $oi ) {
+                                        $d[ 'Author' ] = explode( '/', preg_replace("/^By\s/i","", $os[$oi]) )[0];
+                                } else if ( 1 == $oi ) {
+                                        if (empty($d[ 'Full Text' ])) {
+                                                $d[ 'Full Text' ] = $os[($oi-1)];
+                                        }
+                                        $d[ 'Author' ] = explode( '/', preg_replace("/^By\s/i","", $os[$oi]) )[0];
+                                        unset($os[($oi-1)]);
+                                } else if ( 2 == $oi ) {
+                                        if (empty($d[ 'Full Text' ])) {
+                                                $d[ 'Full Text' ] = $os[($oi-2)];
+                                        }
+                                        $d[ 'Author' ] = explode( '/', preg_replace("/^By\s/i","", $os[$oi]) )[0];
+                                }       
+                        } else {       
+                        	$os[$oi] = preg_replace("/^(n|u)\s/", "* ", $o );
+                        }
+                }
 
 	} else {
 
@@ -80,7 +111,7 @@ function handle_item($filename, $basedir = '') {
 		if ( false !== strpos($cleaned,0004)) {
 			$cleaned = substr($cleaned, 0, strrpos($cleaned, "0004"));
 		}
-		$d = array( 'File' => "$basedir/$filename" );
+
 		$ps = array_filter(explode("\n", $cleaned), function($a){return !empty($a);});
 		$os = array();
 		$m = 0;
@@ -103,9 +134,23 @@ function handle_item($filename, $basedir = '') {
 			}
 		}
 	}
+	if ( isset($d['Author']) && strtoupper($d['Author']) === $d['Author'] && strlen($d['Author']) > 5) {
+		$d['Author'] = ucwords( strtolower( $d['Author'] ) );
+	}
+	if ( isset($d['Author'])) {
+		$d['Author'] = trim($d['Author']);
+	}
+	if ( isset($d['Full Text'])) {
+		$d['Full Text'] = trim($d['Full Text']);
+	}
+	$content = trim( implode("\r\n\n", $os) );
+	if ( !empty($d['Full Text']) && empty( $content ) ) {
+		$content = $d['Full Text'];
+		unset($d['Full Text']);
+	}
 	$results = array( 'data' => array(
 		'meta' => $d,
-		'content' => implode("\r\n\n", $os)
+		'content' => $content
 		), 'updated' => time()
 	);
 	var_dump($results);
@@ -118,11 +163,12 @@ function handle_item($filename, $basedir = '') {
 	$month = date('m', $pub);
 	$day = date('d', $pub);
 	@mkdir("results/$year/$month/$day/",0755,true);
+	@mkdir("yearly/$year/",0755,true);
 	file_put_contents("yearly/$year/$md5.json", json_encode($results));
-	#file_put_contents("results.json", json_encode($results), APPEND_FILE | LOCK_EX);
-	#echo "yearly/$year/$month/$day/$md5.json\n";
+	file_put_contents("results/$year/$month/$day/$md5.json", json_encode($results));
 }
 
+//$filenames = array( "DEArchive", "Stories1", "Stories2", "Stories3", "Stories4" );
 $filenames = array( "samples" );
 foreach($filenames as $filename) {
 	handle_dir($filename);
